89024cd9588b357392b419e83f227a4d3ef7360f
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-17 23:31:10 +0300

    bug fix for todo validation

--- src/todoManager.js
+++ src/todoManager.js
@@ -2,15 +2,15 @@ import Todo from "./todo";
 
 export function createTodo(todoParams, projectIndex) {
   if (validateTodo(todoParams)) {
-    const newTodo = new Todo();
+    const newTodo = new Todo(todoParams);
 
     // add todo to project -- how to make a relationship between array index and a dom element?  this question is for the future reference
-    
+
   }
 }
 
 function validateTodo(todoParams) {
-  if (!(todoParams.desc || priority)) {
+  if (!(todoParams.desc && priority)) {
     console.log("Please fill out the necessary fields.");
     return false;
   }
