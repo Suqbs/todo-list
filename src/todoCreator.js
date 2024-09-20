6d77fcb8a4d61a86c83f4749dbf7ed0760e29dc5
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-09-20 19:24:18 +0300

    Change todo manager to todoCreator

---
+++ src/todoCreator.js
@@ -0,0 +1,16 @@
+import Todo from "./todo";
+
+export function createTodo(todoParams) {
+  if (validateTodo(todoParams)) {
+    return new Todo(todoParams);
+  }
+}
+
+function validateTodo(todoParams) {
+  if (!(todoParams.desc && todoParams.priority)) {
+    console.log("Please fill out the necessary fields.");
+    return false;
+  }
+
+  return true;
+}
