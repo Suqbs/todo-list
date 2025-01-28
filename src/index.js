db72c98b287850e8d3b0b9517d7e41de8052b6af
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2025-01-28 14:16:50 +0300

    Fix UI Problems

--- src/index.js
+++ src/index.js
@@ -25,44 +25,4 @@ const day = formatISO(new Date(), { representation: "date" });
 console.log(day);
 // //#endregion
 
-//#region Example Param Objects
-const ExampleTodo = {
-  desc: "asdfsdaf",
-  priority: "Medium",
-  note: "Not",
-  checkBox: true,
-};
-
-const testTodo = {
-  desc: "Selam",
-  priority: "Medium",
-  note: "...",
-  checkBox: false,
-};
-//#endregion
-
-//#region create Todo
-createTodo(ExampleTodo, GetProject(0));
-//#endregion
-
-//#region edit Todo
-editTodo(getTodo(0), testTodo);
-//#endregion
-
-//#region add due date
-addDueDate(getTodo(0), day);
-removeDueDate(getTodo(0));
-//#endregion
-
-//#region Edit Project
-EditProject({ title: "Nereyi nereyi keşfetmek" }, 0);
-//#endregion
-
-addDueDate(getTodo(0), day);
-
-//#endregion
-
-// Consoleda her şey istendiği gibi çalışıyor bir problem yok.
-// Bir dahakinde DOM işlerini halledersin artık.
-
 
